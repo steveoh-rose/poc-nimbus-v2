a7a6bc396ad9019fcf81da75fa751e825acf94c4
Steve Rose <steven@oh-rose.com>
2025-12-03 17:22:46 +1000

    Update github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-})
+  base: "/poc-nimbus-v2",
+});
